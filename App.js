d535df004ce843b3c5a57e056b515789c750061b
yongqi <77aimiao@gmail.com>
2018-11-27 15:28:01 +0800

    add prettier

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n'
   + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n'
-    + 'Shake or press menu button for dev menu',
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
 })
 
 const styles = StyleSheet.create({
@@ -19,18 +14,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 })
 
 export default function App() {
