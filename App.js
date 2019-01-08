a34111046d14fad04a10cddb6aaae5fff75523c2
linchengzzz <1139489076@qq.com>
2019-01-08 20:57:00 +0800

    添加 andorid 启动屏, iOS 需要重新切图

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Provider } from 'mobx-react/native'
+import SplashScreen from 'react-native-splash-screen'
 
 import App from './src/container'
 import store from './src/store'
 
-function Index() {
-  return (
-    <Provider {...store}>
-      <App />
-    </Provider>
-  )
+class Index extends React.Component {
+  componentDidMount(): void {
+    SplashScreen.hide()
+  }
+
+  render(): React.ReactNode {
+    return (
+      <Provider {...store}>
+        <App />
+      </Provider>
+    )
+  }
 }
 
 export default Index
--- android/app/src/main/java/com/coderiver_rn/MainApplication.java
+++ android/app/src/main/java/com/coderiver_rn/MainApplication.java
@@ -3,6 +3,7 @@ package com.coderiver_rn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,8 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage()
+        new MainReactPackage(),
+        new SplashScreenReactPackage(),
+        new RNGestureHandlerPackage()
       );
     }
 
