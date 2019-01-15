848f44a7b907ff74f29a27543e06229717fc60af
yongqi <77aimiao@gmail.com>
2019-01-15 13:33:03 +0800

    feature add guide

--- src/components/RadiusButton/index.js
+++ src/components/RadiusButton/index.js
@@ -1,27 +1,41 @@
 /**
  * 通用圆角 button，未来可以传入自定义 style 控制
+ * @param {func} onPress 回调函数
+ * @param {string} title 按钮文字
+ * @param {object} textStyle 按钮文字样式
+ * @param {object} contentStyle 按钮样式
+ * @param {node} children 按钮里面的组件
  */
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet, ViewPropTypes } from 'react-native'
 
-const RadiusButton = ({ onPress, title }) => (
+const RadiusButton = ({ onPress, title, children, contentStyle, textStyle }) => (
   <TouchableOpacity
-    style={styles.useEmail}
+    style={[styles.useEmail, contentStyle]}
     onPress={onPress}
   >
-    <Text
-      style={styles.useEmailText}
-    >
-      {title}
-    </Text>
+    {
+      children
+      || (
+        <Text style={[styles.useEmailText, textStyle]}>
+          {title}
+        </Text>
+        )
+    }
   </TouchableOpacity>
 )
 
 RadiusButton.propTypes = {
-  title: PropTypes.string.isRequired,
-  onPress: PropTypes.func.isRequired
+  title: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  contentStyle: ViewPropTypes.style,
+  textStyle: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]))
 }
 
 const styles = StyleSheet.create({
@@ -37,7 +51,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     textAlign: 'center',
     color: '#fff'
-  },
+  }
 })
 
 export default RadiusButton
